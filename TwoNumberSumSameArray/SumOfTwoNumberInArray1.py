63d0687c134dac9e6cf5313ee609b42665cb0f9e
SharukhEqbal <Shahrukheqbal@gmail.com>
2021-05-31 22:46:38 +0530

    Update SumOfTwoNumberInArray1.py

--- TwoNumberSumSameArray/SumOfTwoNumberInArray1.py
+++ TwoNumberSumSameArray/SumOfTwoNumberInArray1.py
@@ -5,9 +5,9 @@ import copy
 # O(n^2) time | O(1) space
 def twoNumberSum1(arr, sum):
     for i in range(len(arr) - 1):
-        for j in range(i, len(arr)):
+        for j in range(i+1, len(arr)):
             if arr[i] + arr[j] == sum:
-                return arr[i], arr[j]
+                return i, j, arr[i], arr[j]
     return []
 
 # [3, 5, -4, 8, 11, 1, -1, 6] targetSum: 10
